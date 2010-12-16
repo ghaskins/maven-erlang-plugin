df1784c214b46f59270579af0c0f059101822573
schlagert <schlagert@9dd1a6f4-3624-4ca5-b421-8c9e39e94436>
2010-12-16 07:31:07 +0000

    removed unused code

--- src/main/java/eu/lindenbaum/maven/util/ErlUtils.java
+++ src/main/java/eu/lindenbaum/maven/util/ErlUtils.java
@@ -1,11 +1,8 @@
 package eu.lindenbaum.maven.util;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 
 import com.ericsson.otp.erlang.OtpErlangAtom;
 import com.ericsson.otp.erlang.OtpErlangInt;
@@ -15,10 +12,6 @@ import com.ericsson.otp.erlang.OtpErlangObject;
 import com.ericsson.otp.erlang.OtpErlangRangeException;
 import com.ericsson.otp.erlang.OtpErlangString;
 import com.ericsson.otp.erlang.OtpErlangUInt;
-
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
 
 /**
  * Containing utilities related to erlang code execution.
@@ -182,117 +175,4 @@ public final class ErlUtils {
     }
     return 0;
   }
-
-  /**
-   * Evaluate an erlang expression and return the result.
-   * 
-   * @param log logger.
-   * @param expression the expression to evaluate.
-   * @return the output of the erl interpreter.
-   */
-  public static String eval(Log log, String expression) throws MojoExecutionException, MojoFailureException {
-    return eval(log, expression, null);
-  }
-
-  /**
-   * Evaluate an erlang expression and return the result.
-   * 
-   * @param log logger
-   * @param expression the expression to evaluate.
-   * @param libPaths list of paths to add to the path, maybe {@code null}
-   * @return the output of the erl interpreter.
-   */
-  public static String eval(Log log, String expression, List<File> libPaths) throws MojoExecutionException,
-                                                                            MojoFailureException {
-    return eval(log, expression, libPaths, null);
-  }
-
-  /**
-   * Evaluate an erlang expression and return the result.
-   * 
-   * @param log logger
-   * @param expression the expression to evaluate.
-   * @param libPaths list of paths to add to the path, maybe {@code null}
-   * @param workingDir the working directory for the spawned process, maybe
-   *          {@code null}
-   * @return the output of the erl interpreter.
-   */
-  public static String eval(final Log log, String expression, List<File> libPaths, File workingDir) throws MojoExecutionException,
-                                                                                                   MojoFailureException {
-    log.debug("Evaluating <<" + expression + ">>");
-    final List<String> command = new LinkedList<String>();
-    command.add(ErlConstants.ERL);
-    if (libPaths != null) {
-      for (File lib : libPaths) {
-        command.add("-pa");
-        command.add(lib.getAbsolutePath());
-      }
-    }
-    command.add("-eval");
-    command.add(expression);
-    command.add("-noshell");
-    command.add("-s");
-    command.add("init");
-    command.add("stop");
-
-    return exec(command, log, workingDir, new Observer() {
-      @Override
-      public String handle(int exitValue, String result) throws MojoExecutionException {
-        if (exitValue != 0) {
-          log.error("Process returned: " + exitValue + " result is: " + result);
-          throw new MojoExecutionException("Error evaluating expression " + command);
-        }
-        return result;
-      }
-    });
-  }
-
-  /**
-   * Executes the given command array in the given working directory. After
-   * process completion the given {@link Observer} is notfied to process the
-   * result.
-   * 
-   * @param commands to execute
-   * @param log used to log errors
-   * @param workingDir the working directory for the spawned process, maybe
-   *          {@code null}
-   * @param observer to be notified on process completion
-   */
-  public static String exec(List<String> commands, final Log log, File workingDir, Observer observer) throws MojoExecutionException,
-                                                                                                     MojoFailureException {
-    log.debug("Executing " + commands.toString());
-    try {
-      ProcessBuilder processBuilder = new ProcessBuilder(commands);
-      processBuilder.directory(workingDir);
-      log.debug("Working directory " + processBuilder.directory());
-      Process process = processBuilder.start();
-      final LinkedList<String> output = new LinkedList<String>();
-      Thread gobbler1 = new Thread(new StreamGobbler(process.getInputStream(), new Processor() {
-        @Override
-        public void handle(String line) {
-          log.info(line);
-          output.addLast(line);
-        }
-      }));
-      Thread gobbler2 = new Thread(new StreamGobbler(process.getErrorStream(), new Processor() {
-        @Override
-        public void handle(String line) {
-          log.error(line);
-        }
-      }));
-      gobbler1.start();
-      gobbler2.start();
-      process.waitFor();
-      gobbler1.join();
-      gobbler2.join();
-      return observer.handle(process.exitValue(), output.isEmpty() ? null : output.getLast());
-    }
-    catch (IOException e) {
-      throw new MojoExecutionException(e.getMessage(), e);
-    }
-    catch (InterruptedException e) {
-      throw new MojoExecutionException(e.getMessage(), e);
-    }
-  }
-
 }
--- src/test/java/eu/lindenbaum/maven/util/ErlUtilsTest.java
+++ src/test/java/eu/lindenbaum/maven/util/ErlUtilsTest.java
@@ -1,7 +1,6 @@
 package eu.lindenbaum.maven.util;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -11,16 +10,9 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.junit.Test;
 
 public class ErlUtilsTest {
-  private static final Log log = new SystemStreamLog();
-
-  private static final String DIR_COMMAND = "dir";
-  private static final String LS_COMMAND = "ls";
-
   @Test
   public void testToListNull() {
     String result = ErlUtils.toList((String[]) null, null, "1", "2");
@@ -50,7 +42,6 @@ public class ErlUtilsTest {
     List<File> list = Arrays.asList(new File("pom.xml"), new File("non_existent"), new File("lgpl.txt"));
     String result = ErlUtils.toFileList(list, "1", "2");
     Pattern pattern = Pattern.compile("\\[1.+pom.xml2, 1.+lgpl.txt2\\]");
-    System.out.println(result);
     Matcher matcher = pattern.matcher(result);
     assertTrue(matcher.matches());
   }
@@ -59,43 +50,5 @@ public class ErlUtilsTest {
   public void testToModuleListNull() {
     String result = ErlUtils.toModuleList(new ArrayList<File>(), "1", "2");
     assertEquals("[]", result);
-  }
-
-  @Test
-  public void testExec() throws Exception {
-    String dir = getSystemSpecificListCommand();
-    List<String> command = Arrays.asList(new String[]{ dir });
-    assertEquals("ok", ErlUtils.exec(command, log, null, new Observer() {
-      @Override
-      public String handle(int exitValue, String result) {
-        assertEquals(exitValue, 0);
-        assertNotNull(result);
-        return "ok";
-      }
-    }));
-  }
-
-  private String getSystemSpecificListCommand() {
-    if ("Mac OS X".equals(System.getProperty("os.name"))) {
-      return LS_COMMAND;
-    }
-    else {
-      return DIR_COMMAND;
-    }
-  }
-
-  @Test
-  public void testEval2() throws Exception {
-    assertEquals("ok", ErlUtils.eval(log, "io:format(\"ok~n\")"));
-  }
-
-  @Test
-  public void testEval3() throws Exception {
-    assertEquals("ok", ErlUtils.eval(log, "io:format(\"ok~n\")", null));
-  }
-
-  @Test
-  public void testEval4() throws Exception {
-    assertEquals("ok", ErlUtils.eval(log, "io:format(\"ok~n\")", null, null));
   }
 }
